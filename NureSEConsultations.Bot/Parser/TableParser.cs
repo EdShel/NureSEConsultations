9b6f1e7390e7814112eb35e235425533ec37fb62
EdShel <eduardchelemetjev@gmail.com>
2021-04-18 18:26:12 +0300

    Init project with parsing consultations table

---
+++ NureSEConsultations.Bot/Parser/TableParser.cs
@@ -0,0 +1,255 @@
+﻿using NureSEConsultations.Bot.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NureSEConsultations.Bot.Parser
+{
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> SplitByNewLine(this string value)
+        {
+            return Regex
+                .Split(value, @"((\r)+)?(\n)+((\r)+)?")
+                .Select(gr => gr.Trim())
+                .Where(gr => !string.IsNullOrEmpty(gr));
+        }
+    }
+
+    public interface IRowsExtender
+    {
+        IEnumerable<IList<string>> SplitRowOnManyRows(IList<string> initialRow);
+    }
+
+    public interface IRowMapper<T>
+    {
+        T Map(IList<string> row);
+    }
+
+    public interface IRowsUnmerger
+    {
+        IList<IList<string>> FillSkippedByMergeValues(IList<IList<string>> table);
+    }
+
+    public class KeyBoundUnmerger : IRowsUnmerger
+    {
+        private readonly int keyColumnIndex;
+
+        public KeyBoundUnmerger(int keyColumnIndex)
+        {
+            this.keyColumnIndex = keyColumnIndex;
+        }
+
+        public IList<IList<string>> FillSkippedByMergeValues(IList<IList<string>> table)
+        {
+            var rowsList = new List<IList<string>>(table.Count);
+            IList<string> previousKeyedRow = null;
+            foreach (var row in table)
+            {
+                if (!string.IsNullOrEmpty(row[this.keyColumnIndex]))
+                {
+                    previousKeyedRow = row;
+                    rowsList.Add(row);
+                }
+                else if (previousKeyedRow != null)
+                {
+                    var rowToAdd = new string[row.Count];
+                    for (int colIndex = 0; colIndex < row.Count; colIndex++)
+                    {
+                        string columnValue = row[colIndex];
+                        string valueToApply;
+                        if (!string.IsNullOrEmpty(columnValue))
+                        {
+                            valueToApply = columnValue;
+                        }
+                        else
+                        {
+                            valueToApply = previousKeyedRow[colIndex];
+                        }
+                        rowToAdd[colIndex] = valueToApply;
+                    }
+                    rowsList.Add(rowToAdd);
+                }
+            }
+            return rowsList;
+        }
+    }
+
+    public class CuratorMeetingConsultationMapper : IRowMapper<Consultation>
+    {
+        private readonly string subjectName;
+
+        public CuratorMeetingConsultationMapper(string subjectName)
+        {
+            this.subjectName = subjectName;
+        }
+
+        public Consultation Map(IList<string> row)
+        {
+            return new Consultation
+            {
+                Subject = subjectName,
+                Teacher = row[0],
+                Group = row[2],
+                Time = row[3],
+                Link = row[4]
+            };
+        }
+    }
+    public class SubjectConsultationMapper : IRowMapper<Consultation>
+    {
+        public Consultation Map(IList<string> row)
+        {
+            return new Consultation
+            {
+                Subject = row[0],
+                Teacher = row[1],
+                Group = row[2],
+                Time = row[3],
+                Link = row[4]
+            };
+        }
+    }
+
+    public class GroupNameRowsExtender : IRowsExtender
+    {
+        private readonly int groupNameColumnIndex;
+
+        public GroupNameRowsExtender(int groupNameColumnIndex)
+        {
+            this.groupNameColumnIndex = groupNameColumnIndex;
+        }
+
+        public IEnumerable<IList<string>> SplitRowOnManyRows(IList<string> columns)
+        {
+            string groupValue = columns[this.groupNameColumnIndex];
+            var allUniqueGroups = groupValue.SplitByNewLine()
+                .SelectMany(gr => SplitCommaSeparatedGroupsRange(gr))
+                .DefaultIfEmpty(string.Empty);
+            return allUniqueGroups.Select(groupName =>
+            {
+                var rowCopy = columns.ToList();
+                rowCopy[this.groupNameColumnIndex] = groupName;
+                return rowCopy;
+            });
+        }
+
+        private IEnumerable<string> SplitCommaSeparatedGroupsRange(string commaSeparatedGroups)
+        {
+            // Regex for formats
+            // ПЗПІ-18-1
+            // ПЗПІ-18-1,3,4
+            // ПЗПІ-18-(4,5,6)
+            var groupRegex = new Regex(@"^(.+-)\(?(.+)\)?$");
+            var groupMatch = groupRegex.Match(commaSeparatedGroups);
+            if (!groupMatch.Success)
+            {
+                yield return commaSeparatedGroups;
+            }
+            string groupPreffix = groupMatch.Groups[1].Value;
+            var groupsNumbers = groupMatch.Groups[2].Value.Split(',');
+            foreach (var groupNumber in groupsNumbers.Where(gr => !string.IsNullOrWhiteSpace(gr)))
+            {
+                yield return groupPreffix + groupNumber;
+            }
+        }
+    }
+
+    public class ConsultationTimeRowsExtender : IRowsExtender
+    {
+        private readonly int timeColumnIndex;
+
+        public ConsultationTimeRowsExtender(int timeColumnIndex)
+        {
+            this.timeColumnIndex = timeColumnIndex;
+        }
+
+        public IEnumerable<IList<string>> SplitRowOnManyRows(IList<string> initialRow)
+        {
+            string timeValue = initialRow[timeColumnIndex];
+            var uniqueTimeRecords = timeValue.SplitByNewLine()
+                .SelectMany(time => time.Split(';'))
+                .Select(time => time.Trim())
+                .Where(time => !string.IsNullOrWhiteSpace(time))
+                .DefaultIfEmpty(string.Empty);
+            return uniqueTimeRecords.Select(time =>
+            {
+                var rowCopy = initialRow.ToList();
+                rowCopy[timeColumnIndex] = time;
+                return rowCopy;
+            });
+        }
+    }
+
+    public class TableParser<T>
+    {
+        private IRowMapper<T> rowMapper;
+
+        public TableParser(IRowMapper<T> rowMapper)
+        {
+            this.rowMapper = rowMapper;
+        }
+
+        private IEnumerable<IRowsExtender> rowExtenders = Enumerable.Empty<IRowsExtender>();
+
+        private IRowsUnmerger unmerger = new KeyBoundUnmerger(0);
+
+        public IRowMapper<T> RowMapper
+        {
+            get => this.rowMapper;
+            set => this.rowMapper = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public IEnumerable<IRowsExtender> RowExtenders
+        {
+            get => this.rowExtenders;
+            set => this.rowExtenders = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        public IRowsUnmerger Unmerger
+        {
+            get => this.unmerger;
+            set => this.unmerger = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public IEnumerable<T> ParseTable(IList<IList<object>> tableData)
+        {
+            var stringified = StringifyData(tableData);
+            var normalized = NormalizeCountOfColumns(stringified, string.Empty);
+            var putSkipped = this.unmerger.FillSkippedByMergeValues(normalized);
+            var extended = putSkipped.SelectMany(r => GetMaximumExtended(r));
+            var mapped = extended.Select(r => this.rowMapper.Map(r));
+            return mapped.ToList();
+        }
+
+        private IList<IList<T>> NormalizeCountOfColumns<T>(IList<IList<T>> table, T defaultValue)
+        {
+            int maxColumnsCount = table.Max(row => row.Count);
+            return table.Select(row =>
+                (IList<T>)row.Concat(Enumerable.Repeat(defaultValue, maxColumnsCount - row.Count))
+                    .ToList()
+            ).ToList();
+        }
+
+        protected virtual IList<IList<string>> StringifyData(IList<IList<object>> tableData)
+        {
+            var cleanupRegex = new Regex(@"\s{2,}");
+            return tableData.Select(row =>
+                (IList<string>)row.Select(col =>
+                {
+                    return cleanupRegex.Replace(col.ToString(), " ").Trim();
+                }).ToList()
+            ).ToList();
+        }
+
+        private IList<IList<string>> GetMaximumExtended(IList<string> row)
+        {
+            IEnumerable<IList<string>> rows = new[] { row };
+            foreach (var extender in this.rowExtenders)
+            {
+                rows = rows.SelectMany(r => extender.SplitRowOnManyRows(r));
+            }
+            return rows.ToList();
+        }
+    }
+}
---
+++ NureSEConsultations.Bot/Program.cs
@@ -0,0 +1,79 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using NureSEConsultations.Bot.Entities;
+using NureSEConsultations.Bot.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NureSEConsultations.Bot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            const string spreadsheetUrl = "https://docs.google.com/spreadsheets/d/160pVT-z-OGnpgdlPQFwfJKmsXVb_N1C1NEBTZbcQOGo/edit#gid=886702434";
+
+            const string clientId = "778237814285-s3ku69pua41b4v6fkh4eatg66j4fm4gp.apps.googleusercontent.com";
+            const string clientSecret = "nYWD6gXs4weNep5Bqb-eowpR";
+
+            const string credsPath = "tokens.json";
+            using var credStream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read);
+            var creds = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.Load(credStream).Secrets,
+                new[] { SheetsService.Scope.SpreadsheetsReadonly },
+                "user",
+                CancellationToken.None,
+                new FileDataStore(credsPath, true)
+            ).Result;
+            Console.WriteLine("Saved creds");
+
+            var sheets = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = creds,
+                ApplicationName = nameof(NureSEConsultations)
+            });
+
+            String spreadsheetId = "160pVT-z-OGnpgdlPQFwfJKmsXVb_N1C1NEBTZbcQOGo";
+
+            String range = "3 курс, ПЗПІ-18!A6:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    sheets.Spreadsheets.Values.Get(spreadsheetId, range);
+            ValueRange response = request.Execute();
+            IList<IList<object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                TableParser<Consultation> tableParser;
+                //tableParser = new TableParser<Consultation>(new CuratorMeetingConsultation("Зустріч з куратором"))
+                //{
+                //    RowExtenders = new IRowsExtender[] { new GroupNameRowsExtender(2), new ConsultationTimeRowsExtender(3) },
+                //};
+                tableParser = new TableParser<Consultation>(new SubjectConsultationMapper())
+                {
+                    RowExtenders = new IRowsExtender[] { new GroupNameRowsExtender(2), new ConsultationTimeRowsExtender(3) },
+                };
+                var consultations = tableParser.ParseTable(values);
+                foreach (var cons in consultations)
+                {
+                    Console.WriteLine($"{cons.Subject} - {cons.Teacher} - {cons.Group} - {cons.Time} - {cons.Link}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+
+
+            //const string token = "1794463891:AAEtc5EU6CkaTxfBuofZBy7EUKGtj9y8G_o";
+            //var botClient = new TelegramBotClient(token);
+            //var me = await botClient.GetMeAsync();
+            //Console.WriteLine($"{me.FirstName}");
+        }
+    }
+}
